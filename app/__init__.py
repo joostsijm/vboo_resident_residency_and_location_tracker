b351ef00c8ebd84a9d962a599765b4fe1ae05c59
JoostSijm <joostsijm@gmail.com>
2019-11-12 23:45:01 +0100

    Iprove logger with log file

--- app/__init__.py
+++ app/__init__.py
@@ -21,11 +21,21 @@ Session = sessionmaker(bind=engine)
 scheduler = BackgroundScheduler()
 scheduler.start()
 
-logging.basicConfig(
-    format='%(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
 LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+# create file handler which logs even debug messages
+FILE_HANDLER = logging.FileHandler('irlt.log')
+FILE_HANDLER.setLevel(logging.INFO)
+# create console handler with a higher log level
+STREAM_HANDLER = logging.StreamHandler()
+STREAM_HANDLER.setLevel(logging.INFO)
+# create formatter and add it to the handlers
+FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+STREAM_HANDLER.setFormatter(FORMATTER)
+FILE_HANDLER.setFormatter(FORMATTER)
+# add the handlers to logger
+LOGGER.addHandler(STREAM_HANDLER)
+LOGGER.addHandler(FILE_HANDLER)
 
 # api
 BASE_URL = os.environ["API_URL"]
